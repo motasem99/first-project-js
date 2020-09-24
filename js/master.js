26647543a7a6fc5ce2574db616ab9aeb7dec48fd
mutasem <motasemimad10@gmail.com>
2020-09-24 16:09:51 +0300

    Create Navigation Bullets Logic

--- js/master.js
+++ js/master.js
@@ -227,3 +227,14 @@ document.addEventListener('click', (e) => {
     document.querySelector('.popup-overlay').remove();
   }
 });
+
+// select all bullets
+const allBullets = document.querySelectorAll('.nav-bullets .bullet');
+
+allBullets.forEach((bullet) => {
+  bullet.addEventListener('click', (e) => {
+    document.querySelector(e.target.dataset.section).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
+});
